c7d07abf80b567deeae5573b976a2571d7a42726
seongmin36 <whtjdals3625@gmail.com>
2026-05-02 02:34:14 +0900

    delete(todo-tags): todo-tags 안티패턴 제거

--- apps/server/src/todo-tags/dto/update-todo-tag.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTodoTagDto } from './create-todo-tag.dto';
-
-export class UpdateTodoTagDto extends PartialType(CreateTodoTagDto) {}
--- apps/server/src/todo-tags/todo-tags.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TodoTagsService } from './todo-tags.service';
-import { TodoTagsController } from './todo-tags.controller';
-
-@Module({
-  controllers: [TodoTagsController],
-  providers: [TodoTagsService],
-})
-export class TodoTagsModule {}
